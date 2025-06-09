edc4bf9a6c875df4a25a9dfe66a233c9e0342f28
moshik <moshik0527669169@gmail.com>
2025-06-09 15:56:44 +0300

    add files pepole and intelreports

---
+++ malshinon_project/intelreports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace malshinon
+{
+    public class Intelreports
+    {
+        public string Text { get; set; }
+        public Intelreports(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
