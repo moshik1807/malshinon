d510fb0acff5deaba00b4b27d475204a33724f5d
moshik <moshik0527669169@gmail.com>
2025-06-10 23:46:51 +0300

    add calculationFunctions file

---
+++ malshinon_project/dal/CalculationFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace malshinon
+{
+    public class CalculationFunctions
+    {
+        public CalculationFunctions()
+        {
+        }
+        public bool checkIfTarget(Pepole pepole)
+        {
+
+        }
+        public bool checkIfReporter(Pepole pepole)
+        {
+
+        }
+    }
+
+}
